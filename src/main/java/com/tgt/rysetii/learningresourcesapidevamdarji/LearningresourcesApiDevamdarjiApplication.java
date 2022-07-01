49629622e4fcacb2fd2c0dcfd627ba5cc2ce0ec3
devam darji <devamdarji22@gmail.com>
2022-07-01 14:48:38 +0530

    Hello world added

---
+++ src/main/java/com/tgt/rysetii/learningresourcesapidevamdarji/LearningresourcesApiDevamdarjiApplication.java
@@ -0,0 +1,14 @@
+package com.tgt.rysetii.learningresourcesapidevamdarji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningresourcesApiDevamdarjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningresourcesApiDevamdarjiApplication.class, args);
+		System.out.println("Hello Target!");
+	}
+
+}
---
+++ src/test/java/com/tgt/rysetii/learningresourcesapidevamdarji/LearningresourcesApiDevamdarjiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningresourcesapidevamdarji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningresourcesApiDevamdarjiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
